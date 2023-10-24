bcc0df148bf5ec479ccee025a4a4800ce9a46e8e
Anna N Helms <unicornbuddha@gmail.com>
2023-10-24 16:47:40 -0700

    Add migration

--- db/migrate/20231024233951_create_products.rb
+++ db/migrate/20231024233951_create_products.rb
@@ -1,7 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-      t.string :
       t.string :name
       t.integer :price
       t.string :image_url
