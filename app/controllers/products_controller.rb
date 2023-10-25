63c8e03d1fe2adc94d21e34f343880b1a40bee10
Anna N Helms <unicornbuddha@gmail.com>
2023-10-24 18:08:05 -0700

    Adding product route and controller by id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,18 +9,23 @@ class ProductsController < ApplicationController
         render json: products.as_json
     end
 
-      def first_product
-        products = Product.first
-        render json: products.as_json
-    end
+    #   def first_product
+    #     products = Product.first
+    #     render json: products.as_json
+    # end
 
-    def second_product
-        products = Product.second
-        render json: products.as_json
-    end
+    # def second_product
+    #     products = Product.second
+    #     render json: products.as_json
+    # end
 
-    def third_product
-        products = Product.third
+    # def third_product
+    #     products = Product.third
+    #     render json: products.as_json
+    # end
+
+    def one_product
+        product = Product.find_by(id: params["id"])
         render json: products.as_json
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "get_all_products"
 
   #short way
-  get "/first_product" => "products#first_product"
-  get "/second_product" => "products#second_product"
-  get "/third_product" => "products#third_product"
+  # get "/first_product" => "products#first_product"
+  # get "/second_product" => "products#second_product"
+  # get "/third_product" => "products#third_product"
+  get "/one_product/:id" => "products#one_product"
 end
