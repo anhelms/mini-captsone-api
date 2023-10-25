6374540d1c8cfc2be95836d6db6fd31efdfb562b
Anna N Helms <unicornbuddha@gmail.com>
2023-10-24 18:33:05 -0700

    Controller fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,26 +6,26 @@ class ProductsController < ApplicationController
 
     def get_all_products
         products = Product.all
-        render json: products.as_json
+        render json: product.as_json
     end
 
     #   def first_product
     #     products = Product.first
-    #     render json: products.as_json
+    #     render json: product.as_json
     # end
 
     # def second_product
     #     products = Product.second
-    #     render json: products.as_json
+    #     render json: product.as_json
     # end
 
     # def third_product
     #     products = Product.third
-    #     render json: products.as_json
+    #     render json: product.as_json
     # end
 
     def one_product
         product = Product.find_by(id: params["id"])
-        render json: products.as_json
+        render json: product.as_json
     end
 end
