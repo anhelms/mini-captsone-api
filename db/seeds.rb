7fce060fd00586fd2449dac56f737882667622ed
Anna N Helms <unicornbuddha@gmail.com>
2023-10-31 18:53:56 -0700

    seeds have dumped

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-puts "Seeds are seeding..."
-supplier_one = Supplier.create(name: "Monrovia", email: "plantsrule@example.com", phone_number: "555-555-5555")
-supplier_one.save
-supplier_two = Supplier.create(name: "Lowes", email: "betterthanhomedepot@example.com", phone_number: "444-444-4444")
-supplier_two.save
-supplier_three = Supplier.create(name: "Silverlake Botany", email: "slbotany@example.com", phone_number: "222-222-2222")
-supplier_three.save
-# product = Product.create!(supplier_id: supplier_one.id, name: "Monstera", price: 35.00 , image_url: "https://m.media-amazon.com/images/I/71jMNU6tWdL._AC_SX679_.jpg", description: "The Swiss cheese plant, or Mexican breadfruit (Monstera deliciosa), is a common houseplant with showy, glossy, perforated leaves slashed to the margins; numerous horticultural varieties have been developed. When fully ripe, its sweet scaly fruit is edible and tastes like a combination of pineapple and mango. All other parts of the plant, including the unripe fruit, contain calcium oxalate and are considered mildly toxic if ingested by humans.")
-product = Product.create!(supplier_id: supplier_two.id, name: "Silver Satin Pothos", price: 10.00 , image_url: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRcY-ETWWVoi1LKsG-zCZGd3xMdVMw1oz6j0ctQGK6poK5WZC53Gqht84lZg51PIiLbd-XB3EX3F419T8CPrG27XoaAJRylnY2mXdiruGd4M87dvwtqZhKopQ&usqp=CAE", description: " With tear or heart-shaped leaves, the Silver Satin Pothos puts out colorful and satin-like foliage. Its soft texture and vining nature add to the aesthetic appeal of this houseplant. Capable of growing 4-8 feet long while indoors, this plant can be a fun addition to your home.")
-product = Product.create!(supplier_id: supplier_three.id, name: "Saguaro Cactus", price: 155.00 , image_url: "https://www.cactusoutlet.com/cdn/shop/products/CM-Saguaro-Product-Main-V1.0_5000x.jpg?v=1643687569", description: "Also known as Carnegia gigantea. The massive iconic sentinel of the Sonoran desert grows up to 30 feet tall and features multiple arms. This cactus can handle moderate periods of frost down to 25 degrees fahrenheit. Available shipped to your door from one foot tall all the way up to six feet tall.")
-puts "All done!"
+Supplier.create!([
+  {name: "Monrovia", email: "plantsrule@example.com", phone_number: "555-555-5555"},
+  {name: "Lowes", email: "betterthanhomedepot@example.com", phone_number: "444-444-4444"},
+  {name: "Silverlake Botany", email: "slbotany@example.com", phone_number: "222-222-2222"},
+  {name: "Monrovia", email: "plantsrule@example.com", phone_number: "555-555-5555"},
+  {name: "Lowes", email: "betterthanhomedepot@example.com", phone_number: "444-444-4444"},
+  {name: "Silverlake Botany", email: "slbotany@example.com", phone_number: "222-222-2222"}
+])
+Product.create!([
+  {name: "Monstera", price: "35.0", image_url: "https://m.media-amazon.com/images/I/71jMNU6tWdL._AC_SX679_.jpg", description: "The Swiss cheese plant, or Mexican breadfruit (Monstera deliciosa), is a common houseplant with showy, glossy, perforated leaves slashed to the margins; numerous horticultural varieties have been developed. When fully ripe, its sweet scaly fruit is edible and tastes like a combination of pineapple and mango. All other parts of the plant, including the unripe fruit, contain calcium oxalate and are considered mildly toxic if ingested by humans.", inventory: nil, active_status: true, supplier_id: 1},
+  {name: "Saguaro Cactus", price: "155.0", image_url: "https://www.cactusoutlet.com/cdn/shop/products/CM-Saguaro-Product-Main-V1.0_5000x.jpg?v=1643687569", description: "Also known as Carnegia gigantea. The massive iconic sentinel of the Sonoran desert grows up to 30 feet tall and features multiple arms. This cactus can handle moderate periods of frost down to 25 degrees fahrenheit. Available shipped to your door from one foot tall all the way up to six feet tall.", inventory: nil, active_status: true, supplier_id: 3},
+  {name: "Silver Satin Pothos", price: "10.0", image_url: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRcY-ETWWVoi1LKsG-zCZGd3xMdVMw1oz6j0ctQGK6poK5WZC53Gqht84lZg51PIiLbd-XB3EX3F419T8CPrG27XoaAJRylnY2mXdiruGd4M87dvwtqZhKopQ&usqp=CAE", description: " With tear or heart-shaped leaves, the Silver Satin Pothos puts out colorful and satin-like foliage. Its soft texture and vining nature add to the aesthetic appeal of this houseplant. Capable of growing 4-8 feet long while indoors, this plant can be a fun addition to your home.", inventory: nil, active_status: true, supplier_id: 2}
+])
