b153af9a227188d7dd9432be52c60ebc9edbeb58
Anna N Helms <unicornbuddha@gmail.com>
2023-11-01 17:31:46 -0700

    Adds image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_01_020811) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_002914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -23,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_01_020811) do
     t.integer "inventory"
     t.boolean "active_status", default: true
     t.integer "supplier_id"
+    t.string "image"
   end
 
   create_table "suppliers", force: :cascade do |t|
