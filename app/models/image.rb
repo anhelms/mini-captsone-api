fabbbfe61c57e9b2b94286f29c2f7627885d3929
Anna N Helms <unicornbuddha@gmail.com>
2023-11-01 17:35:31 -0700

    Adds has_many and belongs_to and moves validation to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,7 @@
 class Image < ApplicationRecord
+    belongs_to :product
+
+    validates_format_of :with => %r{\.(png|jpg|jpeg)$}i, :message => "must have a valid file type", multiline: true
+
 end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,13 +2,10 @@ class Product < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :price, presence: true, numericality: { greater_than: 0 }
     validates :description, presence: true, length: { in: 10..500 }
-    # validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i, :message => "must have a valid file type", multiline: true
 
     belongs_to :supplier
-    # def supplier
-    #     supplier = Supplier.find_by(id: supplier_id)
-    #     return supplier
-    # end
+    has_many :images
+
     def is_discounted?
         price <= 10
     end
