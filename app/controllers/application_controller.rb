cf6b542686dc04d8290fb6c389ebc1b1e860cb89
Anna N Helms <unicornbuddha@gmail.com>
2023-11-19 01:08:12 -0800

    Adds auth protections to orders controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,17 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception, unless: -> { request.format.json? }
     
     def current_user
-        auth_headers = request.header["Authorization"]
+        auth_headers = request.headers["Authorization"]
         if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
             token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
             begin
                 decoded_token = JWT.decode(
-                    toekn,
+                    token,
                     Rails.application.credentials.fetch(:secret_key_base),
                     true,
-                    { alogorithm: "HS256" }
+                    { algorithm: "HS256" }
                 )
-                USer.find_by(id: decoded_token[0]["user_id"])
+                User.find_by(id: decoded_token[0]["user_id"])
             rescue JWT::ExpiredSignature
                 nil
             end
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
 
     def authenticate_user
         unless current_user
-            render json: {}, satus: :unauthorized
+            render json: {}, status: :unauthorized
         end
     end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
-
+ before_action :authenticate_user
+ 
   def index
-    @orders = current_user.orders
-    render :index
+      @orders = current_user.orders
+      render :index
   end
 
   def show
@@ -11,26 +12,25 @@ class OrdersController < ApplicationController
   end
 
   def create
+      product = Product.find_by(id: params[:product_id])
 
-    product = Product.find_by(id: params[:product_id])
+      calculated_subtotal = product.price * params[:quantity].to_i
+      calculated_tax = calculated_subtotal * 0.09
+      calculated_total = calculated_subtotal + calculated_tax
 
-    calculated_subtotal = product.price * params[:quantity].to_i
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
+      @order = Order.create(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: calculated_subtotal,
+        tax: calculated_tax,
+        total: calculated_total
+      )
 
-    @order = Order.create(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total
-    )
-
-    if @order.valid?
-      render :show
-    else
-        render json: { errors: @order.errors.full_messages }, status: 422
+      if @order.valid?
+        render :show
+      else
+          render json: { errors: @order.errors.full_messages }, status: 422
+      end
     end
-  end
 end
